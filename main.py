87014d9a0e05988f2af9ea9b0b750a3143477053
Tronix Kwak <tronixdevelop@gmail.com>
2019-12-18 02:37:09 +0900

    feat: Run python file and send result to channel (#2)
    * feat: Run python file and send result to server
    
    * feat: Run python file and send result to channel
    
    * fix: Handled error CalledProcessError, discord.errors.HTTPException
    * CalledProcessError occurs when user's code throws error.
    * HTTPException occurs when there's no output from user's code
    
    * chore: Delete config.json

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import json
+from subprocess import check_output
+from subprocess import CalledProcessError
 
 with open("config.json") as config_file:
     config = json.load(config_file)
@@ -14,4 +16,37 @@ async def on_ready():
     await client.change_presence(activity=game)
 
 
-client.run(config["token"])
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('message_check '):
+        msg = message.content.split(maxsplit=1)[1]
+        print(msg)
+        await message.channel.send(msg)
+
+    if message.content.startswith('py '):
+        print('---------------------------------------------')
+
+        code = message.content.split(maxsplit=1)[1]
+        print(code)
+        with open('code.py', 'w') as f:
+            f.write(code)
+
+        try:
+            result = check_output('python3 code.py', shell=True, universal_newlines=True)
+        except CalledProcessError:
+            error_message = "Oh, no! Your code was crashed!"
+            await message.channel.send(error_message)
+        else:
+            try:
+                await message.channel.send(result)
+            except discord.errors.HTTPException:
+                print('// null')
+            else:
+                print('// ' + result)
+
+
+client.run(config['token'])
